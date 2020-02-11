add827a2f5826aa3dbef8aa144b813177f3c50b4
Bobby Uno <robert.y.uno@gmail.com>
2020-02-11 20:25:30 +0000

    added a line to see if things are working

---
+++ hello_timeout.js
@@ -0,0 +1,19 @@
+// console.log('4311o');
+// console.log('th3r3');
+// console.log('w0r1d');
+
+
+setTimeout(() => {
+  console.log('4311o');
+}, 1000);
+
+setTimeout(() => {
+  console.log('th3r3');
+}, 2000);
+
+setTimeout(() => {
+  console.log('w0r1d');
+}, 3000);
+
+
+//new stuff
